d59ead5395a8f8d0af529ca41a23621915606c9a
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-23 13:58:42 -0500

    Update and rename gestion.java to Zona.java

---
+++ src/main/java/Zona.java
@@ -0,0 +1,45 @@
+package gestion;
+import java.util.ArrayList;
+import zooAnimales.Animal;
+
+public class Zona {
+    private String nombre;
+    private Zoologico[] zoo = new Zoologico[1];
+    private ArrayList<Animal> animales = new ArrayList<Animal>();
+
+
+    public Zona(){}
+
+    public Zona(String nombre, Zoologico zoo){
+        this.nombre = nombre;
+        this.zoo[0] = zoo;
+    }
+
+
+    public void agregarAnimales (Animal animalNuevo){
+        animales.add(animalNuevo);
+    }
+
+    public int cantidadAnimales (){
+        int numAnimalesEnZona = 0;
+        for (Animal animal : animales){
+            numAnimalesEnZona += 1;
+        }
+        return numAnimalesEnZona;
+    }
+
+    public void setNombreZona (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombreZona (){
+        return this.nombre;
+    }
+
+    public Zoologico getNombreZoo (){
+        return this.zoo[0];
+    }
+
+    public Zoologico getUbicacionZoo (){
+        return this.zoo[1];
+    }
+}
