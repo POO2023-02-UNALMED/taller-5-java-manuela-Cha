92f286af8e5134c38fe262c012783d562fe774a0
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-23 15:35:37 -0500

    Update Reptil.java

--- src/main/java/Reptil.java
+++ src/main/java/Reptil.java
@@ -37,13 +37,13 @@ public class Reptil extends Animal {
         return "reptar";
     }  
 
-    public Reptil crearIguana (String nombre, int edad, String genero){
+    public static Reptil crearIguana (String nombre, int edad, String genero){
         iguanas += 1;
         Reptil iguana = new Reptil (nombre, edad, "humedal", genero, "verde", 3);
         return iguana; 
     }
 
-    public Reptil crearSerpiente (String nombre, int edad, String genero){
+    public static Reptil crearSerpiente (String nombre, int edad, String genero){
         serpientes += 1;
         Reptil serpiente = new Reptil (nombre, edad, "jungla", genero, "blanco", 1);
         return serpiente; 
