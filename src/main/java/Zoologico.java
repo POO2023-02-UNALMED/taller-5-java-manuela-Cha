2325e7dfe98048946c01ac22842d8e40f91f7676
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-23 13:59:40 -0500

    Create Zoologico.java

---
+++ src/main/java/Zoologico.java
@@ -0,0 +1,42 @@
+package gestion;
+import java.util.ArrayList;
+
+
+public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private ArrayList<Zona> zonas = new ArrayList<Zona>();
+
+    public Zoologico(){}
+
+    public Zoologico(String nombre, String ubicacion){
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    public void agregarZonas(Zona zonaNueva){
+        zonas.add(zonaNueva);
+    }
+
+    public int cantidadTotalAnimales(){
+        int totalAnimales = 0;
+        for (Zona zona : zonas){
+             totalAnimales += zona.cantidadAnimales();
+        } 
+        return totalAnimales;
+    }
+
+    public void setNombreZoo (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombreZoo (){
+        return this.nombre;
+    }
+
+    public void setUbicacionZoo (String ubicacion){
+        this.ubicacion = ubicacion;
+    }
+    public String getUbicacionZoo (){
+        return this.ubicacion;
+    }
+}
