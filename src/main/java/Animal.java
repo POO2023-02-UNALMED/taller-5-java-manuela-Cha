3a006d7068bd6e763071fa2f74dacbd009d6b8d8
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-23 18:45:36 -0500

    Update Animal.java

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -65,12 +65,12 @@ public class Animal {
 
 
     public String toString (){
-        if (zona[0] != null && zona[0].getZoo() !=  null){ 
+        if (zona[0] != null && zona[1].getZoo() !=  null){ 
             return "Mi nombre es " + this.nombre + ", tengo una edad de " 
         + this.edad + ", habito en" + this.habitat + " y mi genero es " + this.genero +
-        ", la zona en la que me ubico es " + this.zona[0] +", en el " + this.zona[0].getZoo();
+        ", la zona en la que me ubico es " + this.zona[0] +", en el " + this.zona[1].getZoo();
         }
         return "Mi nombre es " + this.nombre + ", tengo una edad de " 
-        + this.edad + ", habito en " + this.habitat + " y mi genero es" + this.genero;
+        + this.edad + ", habito en " + this.habitat + " y mi genero es " + this.genero;
     }
 }
